ec4a746d26472ed664edb063bcf835a6fbf7c7ef
aalsayad <74960405+aalsayad@users.noreply.github.com>
2022-04-10 23:47:53 +0300

    Added Randomized Meme Images using useState

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,10 +1,17 @@
 import "./Main.css";
-import React from "react";
+import React, { useState } from "react";
 import { FiRepeat } from "react-icons/fi";
 import { FiUpload } from "react-icons/fi";
 import { FiDownload } from "react-icons/fi";
+import memesData from "../../Data/memesData";
 
 function Main() {
+  const [memeURL, setMemeURL] = useState("./images/leonardo-meme.jpg");
+
+  function randomizeMeme() {
+    setMemeURL(memesData.data.memes[Math.floor(Math.random() * memesData.data.memes.length)].url);
+  }
+
   return (
     <div className="container--main">
       <div className="generator-wrapper">
@@ -21,7 +28,7 @@ function Main() {
             <h2>Choose Image</h2>
           </div>
           <div className="btns-wrapper">
-            <button className="btn">
+            <button className="btn" onClick={randomizeMeme}>
               Randomize
               <span className="btn__icon">
                 <FiRepeat size="16px" />
@@ -38,7 +45,7 @@ function Main() {
 
         <div className="line__divider"></div>
 
-        <img className="meme__image" src="./images/leonardo-meme.jpg"></img>
+        <img className="meme__image" src={memeURL}></img>
 
         <button className="btn btn--dark btn--small">
           Download
